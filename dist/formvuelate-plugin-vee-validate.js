490ae686dfcb2f383efb9c6cd3fbe0db071188b6
Abdelrahman Awad <logaretm1@gmail.com>
2020-10-24 15:56:28 +0200

    chore: build dist

--- dist/formvuelate-plugin-vee-validate.js
+++ dist/formvuelate-plugin-vee-validate.js
@@ -19,43 +19,59 @@
  */
 var mapElementsInSchema = function (schema, fn) { return schema.map(function (row) { return row.map(function (el) { return fn(el); }); }); };
 
-function VeeValidatePlugin (baseReturns) {
-  // Take the parsed schema from SchemaForm setup returns
-  var parsedSchema = baseReturns.parsedSchema;
-  var formBinds = baseReturns.formBinds;
-
-  // Get additional properties not defined on the `SchemaForm` derivatives
-  var ref = vue.getCurrentInstance();
-  var formAttrs = ref.attrs;
-  // Create a form context and inject the validation schema if provided
-  var ref$1 = veeValidate.useForm({
-    validationSchema: formAttrs['validation-schema'] || formAttrs['validationSchema']
-  });
-  var handleSubmit = ref$1.handleSubmit;
-
-  // Map components in schema to enhanced versions with `useField`
-  var formSchema = mapElementsInSchema(parsedSchema.value, function (el) {
-    return Object.assign({}, el,
-      {component: vue.markRaw(withField(el.component))})
-  });
-
-  // override the submit function with one that triggers validation
-  var formSubmit = formBinds.value.onSubmit;  
-  var onSubmit = handleSubmit(function (_, ref) {
-    var evt = ref.evt;
-
-    formSubmit(evt);
-  });
-
-  return Object.assign({}, baseReturns,
-    {formBinds: vue.computed(function () {
-      return Object.assign({}, baseReturns.formBinds.value,
-        {onSubmit: onSubmit})
-    }),
-    parsedSchema: vue.computed(function () { return formSchema; })})
+/**
+ * Maps the validation state to props
+ */
+function defaultMapProps(validation, el) {
+  return {
+    validation: validation,
+  };
 }
 
-function withField (Comp) {
+function VeeValidatePlugin(opts) {
+  // Maps the validation state exposed by vee-validate to components
+  var mapProps = (opts && opts.mapProps) || defaultMapProps;
+
+  return function veeValidatePlugin(baseReturns) {
+  // Take the parsed schema from SchemaForm setup returns
+    var parsedSchema = baseReturns.parsedSchema;
+    var formBinds = baseReturns.formBinds;
+
+    // Get additional properties not defined on the `SchemaForm` derivatives
+    var ref = vue.getCurrentInstance();
+    var formAttrs = ref.attrs;
+    // Create a form context and inject the validation schema if provided
+    var ref$1 = veeValidate.useForm({
+      validationSchema: formAttrs['validation-schema'] || formAttrs['validationSchema']
+    });
+    var handleSubmit = ref$1.handleSubmit;
+
+    // Map components in schema to enhanced versions with `useField`
+    var formSchema = mapElementsInSchema(parsedSchema.value, function (el) {
+      return Object.assign({}, el,
+        {component: vue.markRaw(withField(el, mapProps))})
+    });
+
+    // override the submit function with one that triggers validation
+    var formSubmit = formBinds.value.onSubmit;  
+    var onSubmit = handleSubmit(function (_, ref) {
+      var evt = ref.evt;
+
+      formSubmit(evt);
+    });
+
+    return Object.assign({}, baseReturns,
+      {formBinds: vue.computed(function () {
+        return Object.assign({}, baseReturns.formBinds.value,
+          {onSubmit: onSubmit})
+      }),
+      parsedSchema: vue.computed(function () { return formSchema; })})
+  }
+}
+
+function withField(el, mapProps) {
+  var Comp = el.component;
+
   return {
     name: 'withFieldWrapper',
     props: {
@@ -74,14 +90,16 @@ function withField (Comp) {
       var ref$1 = vue.toRefs(props);
       var validations = ref$1.validations;
       var modelValue = ref$1.modelValue;
-      var model = ref$1.model;
-      var fieldName = model ? model.value : attrs.model;
       var initialValue = modelValue ? modelValue.value : undefined;
-      var ref$2 = veeValidate.useField(fieldName, validations || attrs.rules, {
+      var ref$2 = veeValidate.useField(attrs.model, validations, {
         initialValue: initialValue
       });
       var value = ref$2.value;
       var errorMessage = ref$2.errorMessage;
+      var meta = ref$2.meta;
+      var setDirty = ref$2.setDirty;
+      var setTouched = ref$2.setTouched;
+      var errors = ref$2.errors;
       
       if (modelValue) {
         vue.watch(modelValue, function (val) {
@@ -93,7 +111,13 @@ function withField (Comp) {
       return function renderWithField() {
         return vue.h(Comp, Object.assign({}, props,
           attrs,
-          {errorMessage: errorMessage.value}))
+          mapProps({
+            errorMessage: vue.unref(errorMessage),
+            errors: vue.unref(errors),
+            meta: meta,
+            setDirty: setDirty,
+            setTouched: setTouched,
+          }, el)))
       }
     },
   }

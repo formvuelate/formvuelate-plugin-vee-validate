721fe47db9745d1addabfb078da5efaf08e0961c
Jay/Fienna Liang <roackb2@gmail.com>
2021-07-30 17:38:55 +0800

    Geenrate type declaration at build time to support TypeScript

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export default function VeeValidatePlugin(opts: any): (baseReturns: any) => any;
+export function withField(el: any): any;
+export function mapElementsInSchema(schema: any[], fn: Function): any[];
